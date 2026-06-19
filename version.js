06aa01e411898227b03feb9fc53608eabf69ec78
elpalaiso <elpalaiso@gmail.com>
2026-06-19 18:49:33 +0900

    build tag ec29cf7

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-(function(){ var b=document.getElementById('build'); if(b) b.textContent='build f62e72c · 06-19 18:40'; })();
+(function(){ var b=document.getElementById('build'); if(b) b.textContent='build ec29cf7 · 06-19 18:49'; })();
