8892aed319c99abce7a5021936c77286bfeea326
elpalaiso <elpalaiso@gmail.com>
2026-06-19 20:57:28 +0900

    chore: build stamp

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-(function(){ var b=document.getElementById('build'); if(b) b.textContent='build 02ca5fe · 06-19 19:01'; })();
+(function(){ var b=document.getElementById('build'); if(b) b.textContent='build 261058e · 06-19 20:57'; })();
