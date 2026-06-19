c8bb400e213813b840872dd1bb6369c04a0769f6
elpalaiso <elpalaiso@gmail.com>
2026-06-20 02:04:35 +0900

    chore: build stamp

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-(function(){ var b=document.getElementById('build'); if(b) b.textContent='build 96b1b46 · 06-20 01:51'; })();
+(function(){ var b=document.getElementById('build'); if(b) b.textContent='build 25400bb · 06-20 02:04'; })();
